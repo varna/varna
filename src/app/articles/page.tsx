89e0e052f4167edfe2157cb44b8fc472c3525844
Paulius Varna <paul.varna@gmail.com>
2023-10-22 22:50:09 +0300

    debug: edge

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -5,6 +5,8 @@ import { SimpleLayout } from '@/components/SimpleLayout'
 import { type ArticleWithSlug, getAllArticles } from '@/lib/articles'
 import { formatDate } from '@/lib/formatDate'
 
+export const runtime = 'edge' // 'nodejs' (default) | 'edge'
+
 function Article({ article }: { article: ArticleWithSlug }) {
   return (
     <article className="md:grid md:grid-cols-4 md:items-baseline">
